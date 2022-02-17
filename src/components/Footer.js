2bbc4dd89eec9adc271469de4675d1f2e5bd87a6
hgobr <hugo.brothier@hotmail.fr>
2022-02-17 01:26:56 +0100

    update main files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
-  return <footer>some movies</footer>;
+  return (
+    <footer>
+      <div className="footerItems">
+        <img src="./img/popcorn01.png" alt="logo" />
+        <div className="footerText">
+          <h4>© 2022</h4>
+          <p>Some Movies</p>
+        </div>
+      </div>
+
+      <div className="footerLinks">
+        <NavLink to="/">
+          <FontAwesomeIcon icon={faHome} />
+        </NavLink>
+        <NavLink to="/fav">
+          <FontAwesomeIcon icon={faStar} />
+        </NavLink>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/Fav.js
+++ src/pages/Fav.js
@@ -1,38 +1,15 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 
 const About = () => {
   return (
-    <div>
+    <div className="fav">
       <Navigation />
       <Logo />
       <h1>Vos favoris</h1>
-      <br />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa
-        inventore, corrupti consequatur velit sed sapiente vel dicta unde
-        quisquam. Nesciunt sequi corporis at? Saepe velit quidem architecto
-        quibusdam sed ea, iste sapiente a nesciunt modi voluptas earum
-        laudantium alias ipsa assumenda incidunt, rem doloremque id neque
-        repudiandae? Repellendus laborum sunt vel, adipisci impedit sequi dolore
-        quae quidem illum distinctio atquem!
-      </p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae cum
-        doloremque autem placeat, facilis consequatour repellat suscipit
-        voluptatem. Labouriosam explicabo dolores iusto numquam. Earum
-        excepturi, blanditiis repudiandae animi deserunt cum laboriosam
-        perferendis ab commodi ea ullam enim nihil quasi magni laudantium
-        voluptatibus aspernatur recusandae illum neque, reprehenderit
-        consequuntur facilis adipisci expedita assumenda! Cum labore accusamus
-        ullam autem voluptas expedita tenetur repudiandae culpa! Aliquid, est
-        reprehenderit accusamus modi nostrum laudantium facere facilis quidem?
-        Hic velit, nisi error aliquam, repellendus rem dicta id illum labore
-        perspiciatis qui enim possimus. Unde labore harum nobis quis, laboriosam
-        illo non? Ea officia placeat porro deleniti?
-      </p>
+      <Footer />
     </div>
   );
 };
